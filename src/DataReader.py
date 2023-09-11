e0f197aee3955d89b832ff4589ebd5fe8d0672f0
mihsan96 <mihsan395@gmail.com>
2023-09-11 14:12:38 +0300

    created xml reader

--- src/DataReader.py
+++ src/DataReader.py
@@ -6,5 +6,4 @@ from abc import ABC, abstractmethod
 class DataReader(ABC):
     @abstractmethod
     def read(self, path: str) -> DataType:
-
         pass
---
+++ src/DataReaderXML.py
@@ -0,0 +1,39 @@
+from abc import ABC
+from DataReader import DataReader
+from Types import DataType
+import pandas as pd
+import numpy as np
+
+
+class DataReaderXML(DataReader, ABC):
+    def __init__(self, ) -> None:
+        self.students = {}
+
+    @staticmethod
+    def _normalize_student(name, scores) -> DataType:
+        normalized_student = {}
+        normalized_scores = []
+        for subject in scores:
+            score = scores[subject]
+            if not np.isnan(score):
+                normalized_scores.append((subject, score))
+        normalized_student[name] = normalized_scores
+        return normalized_student
+
+    def _get_all_students(self, df) -> DataType:
+        data = df.to_dict(orient='index')
+        for student in data:
+            normalized_student = self._normalize_student(student, data[student])
+            self.students |= normalized_student
+        return self.students
+
+    def read(self, path: str) -> DataType:
+        df = pd.read_xml(path)
+        df.set_index('ФИО', inplace=True)
+        return self._get_all_students(df)
+
+
+if __name__ == "__main__":
+    path_xml = '../data/data.xml'
+    reader = DataReaderXML().read(path_xml)
+    print(reader)
